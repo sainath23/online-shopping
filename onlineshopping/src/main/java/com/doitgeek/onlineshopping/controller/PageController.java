627947e908ffd36a4d4d6649a8e154a903dd631a
Sainath Parkar <saiparkar4@gmail.com>
2018-10-16 02:18:11 +0530

    Completed interface theme and other UI stuffs.

--- onlineshopping/src/main/java/com/doitgeek/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/com/doitgeek/onlineshopping/controller/PageController.java
@@ -1,7 +1,6 @@
 package com.doitgeek.onlineshopping.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -11,17 +10,24 @@ public class PageController {
 	@RequestMapping(value = {"/", "/home", "/index"})
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", "Welcome to Spring MVC Project");
+		mv.addObject("title", "Home");
+		mv.addObject("userClickHome", true);
 		return mv;
 	}
 	
-	@RequestMapping(value = "/test/{greeting}")
-	public ModelAndView test(@PathVariable("greeting") String greeting) {
-		if(greeting == null) {
-			greeting = "SAINATH";
-		}
+	@RequestMapping("/about")
+	public ModelAndView about() {
 		ModelAndView mv = new ModelAndView("page");
-		mv.addObject("greeting", greeting);
+		mv.addObject("title", "About Us");
+		mv.addObject("userClickAbout", true);
+		return mv;
+	}
+	
+	@RequestMapping("/contact")
+	public ModelAndView contact() {
+		ModelAndView mv = new ModelAndView("page");
+		mv.addObject("title", "Contact Us");
+		mv.addObject("userClickContact", true);
 		return mv;
 	}
 
